3671121481aff6b49d170ca1bed9713863b89143
Gulmirza Vahubov <mirivhbv@gmail.com>
2024-03-22 18:10:59 +0400

    Aligned SSL to make gRPC work

--- src/StorageApi/Services/StorageService.cs
+++ src/StorageApi/Services/StorageService.cs
@@ -4,8 +4,16 @@ namespace StorageApi.Services;
 
 public class StorageService : IStorageService
 {
+    private readonly ILogger<StorageService> _logger;
+
+    public StorageService(ILogger<StorageService> logger)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
     public Task StoreAsync(TrackingRequest request)
     {
+        _logger.LogInformation("Store method called");
         return Task.CompletedTask;
     }
 }
